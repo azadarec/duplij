90faeaf41dcf6fb3701a709d2e1f278a76d6c662
azadarec <azadarec@gmail.com>
2022-04-03 21:26:16 +0200

    feat(WIP): added function to get last subdirectories
    Added a function to get the last subdirectory of every directory in every root to then get the file-hashes(wip)
    
    Signed-off-by: azadarec <azadarec@gmail.com>

--- src/main/java/Windows/WFileSystem.java
+++ src/main/java/Windows/WFileSystem.java
@@ -1,25 +1,36 @@
 package Windows;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Windows File System functions for the Application
+ * @author azadarec <azadarec@gmail.com>
  */
 @Getter
+@Slf4j
 public class WFileSystem {
 
     private final File[] paths = File.listRoots();
     private final FileSystemView fsv = FileSystemView.getFileSystemView();
     private HashMap<String, ArrayList<File>> orderedList;
+    private HashMap<String, ArrayList<File>> subdirsByRoot;
 
     public WFileSystem() {
         fillOrderedList();
+        subdirsByRoot = new HashMap<>();
     }
 
     public WFileSystem(boolean sorted) {
@@ -28,7 +39,7 @@ public class WFileSystem {
             return;
         }
         fillOrderedList();
-
+        subdirsByRoot = new HashMap<>();
     }
 
     public void fillOrderedList() {
@@ -52,19 +63,54 @@ public class WFileSystem {
         String[] fileNames;
 
         for(String key : orderedList.keySet()) {
-            ArrayList<File> currentRoot = orderedList.get(key);
-            getAllFilenamesInRoot(currentRoot);
+            ArrayList<File> currentType = orderedList.get(key);
+            iterateFsRoots(currentType);
         }
     }
 
-    private void getAllFilenamesInRoot(@NotNull ArrayList<File> files) {
-//        String[] fileNames;
+    private void iterateFsRoots(@NotNull ArrayList<File> files) {
 
         for(File f : files) {
-            String temp = f.getName();
-            System.out.println(temp);
+            getDirectoryList(f);
         }
 
+    }
+    private void setSubDirsByRoot(String key, File file) {
+
+        ArrayList<File> arrayList;
+        if(subdirsByRoot.get(key) == null) {
+            arrayList = new ArrayList<>();
+        } else {
+            arrayList = subdirsByRoot.get(key);
+        }
+        arrayList.add(file);
+        subdirsByRoot.put(key, arrayList);
+
+    }
+
+    private void getDirectoryList(@NotNull File file) {
+        String rootDir = file.toString();
+        try {
+            Files.walkFileTree(file.toPath(), new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+
+                    File[] subdirectories = dir.toFile().listFiles(File::isDirectory);
+
+                    assert subdirectories != null;
+
+                    if (subdirectories.length == 0) {
+
+                        setSubDirsByRoot(rootDir, dir.toFile());
+                    }
+
+                    return FileVisitResult.CONTINUE;
+
+                }
+            });
+        } catch (IOException e) {
+            log.error(e.toString());
+        }
     }
 
 }
